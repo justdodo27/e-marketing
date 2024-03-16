a4619f13bc51536041634983e7a708a3f324002a
Dominik Sawicki <dominik.sawicki52@gmail.com>
2024-03-16 22:51:22 +0100

    Changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ const colors = require('tailwindcss/colors');
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   site: 'https://justdodo27.github.io',
-  base: '/e-marketing',
+  base: '/',
   darkMode: "class",
 	theme: {
     screens: {
